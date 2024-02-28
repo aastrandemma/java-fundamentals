355c19954065c172007bad82968468cba07a8cd6
Emma Åstrand <aastrand.emma@gmail.com>
2024-02-28 11:43:55 +0100

    Feat: added hello method

---
+++ src/main/java/com/github/aastrandemma/Hello.java
@@ -0,0 +1,10 @@
+package com.github.aastrandemma;
+
+public class Hello
+{
+    public static void main( String[] args )
+    {
+        String name = "Emma";
+        System.out.println( "Hello \n" + name + "!");
+    }
+}
